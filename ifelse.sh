f128b968eac1cbd12693e915cea2b63c9f5a5187
chandureddy08 <chandrareddyerigela@gmail.com>
2025-03-18 15:32:24 -0400

    pushed

--- ifelse.sh
+++ ifelse.sh
@@ -5,9 +5,6 @@ NUMBER=$1
 if [$NUMBER -eq 10]
 then
     echo "$NUMBER is equal to 10"
-        if [$NUMBER -gt 10]
-        then
-            echo "$NUMBER is greate than 10"
 else
     echo "$NUMBER is less than 10"
 fi
