847abc30703f829e3c0b8710d0e548b6b99e6dfb
chandureddy08 <chandrareddyerigela@gmail.com>
2025-03-18 17:05:25 -0400

    pushed

--- function.sh
+++ function.sh
@@ -2,8 +2,8 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPTNAME=$($0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPTNAME/$TIMESTAMP.log
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME/$TIMESTAMP.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
