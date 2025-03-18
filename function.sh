cf7b22e2ea38846b0ff6892ec3cff89bfd8ff00c
chandureddy08 <chandrareddyerigela@gmail.com>
2025-03-18 17:52:16 -0400

    pushed

--- function.sh
+++ function.sh
@@ -12,10 +12,10 @@ N="\[e0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 installation..$R Failure $N"
+        echo -e "$2 installation..$R Failure $N"
         exit 1
     else
-        echo "$2 installation..$G Success $N"
+        echo -e "$2 installation..$G Success $N"
     fi
 }
 
