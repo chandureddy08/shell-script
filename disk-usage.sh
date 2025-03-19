65b81b4b65d47d0a98e1b3f94437b093596012bf
chandureddy08 <chandrareddyerigela@gmail.com>
2025-03-19 18:12:21 -0400

    pushed

---
+++ disk-usage.sh
@@ -0,0 +1,15 @@
+DISK_USAGE=$(df -hT | grep efx )
+DISK_THRESHOLD=75
+
+
+while IFS = read -r line
+do
+    USAGE=$( echo $line | awk -F " " '{print $6}' | cut -d "%" -f1 )
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$FOLDER more than $DISK_THRESHOLD, current usage is $USAGE"
+    else
+        echo "Disk usage is $USAGE and Good"
+    fi
+done <<<$DISK_USAGE
