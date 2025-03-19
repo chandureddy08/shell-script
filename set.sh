60d5ba8c87b963c4a161d094bf94609efcf57bb4
chandureddy08 <chandrareddyerigela@gmail.com>
2025-03-19 13:40:24 -0400

    pushed

---
+++ set.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "You are not a Super user please run with root access"
+    exit 1
+else
+    echo "You are a super user"
+fi
+
+dnf install mysql -y
+
+dnf install git -y
+
+dnf install huhbh -y
+
+echo "THE END"
