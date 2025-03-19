bcdd38790a0445704d82f9c81425b74db49c7176
chandureddy08 <chandrareddyerigela@gmail.com>
2025-03-19 13:06:02 -0400

    pushed

--- current.sh
+++ current.sh
@@ -5,7 +5,7 @@ COURSE="DevOps from current Script"
 echo "Before Calling other script, course: $COURSE"
 echo "Process ID of Current shell-script: $$"
 
-#./other.sh
+./other.sh
 
 echo "After Calling other script, course: $COURSE"
 echo "Process ID after calling other shell-script: $$"
